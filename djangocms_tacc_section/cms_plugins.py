79267143f43477c5b2b55b8faef6b2e9b6e09507
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-01-10 15:54:40 -0600

    Test: Give Up Style Model Field References

--- djangocms_tacc_section/cms_plugins.py
+++ djangocms_tacc_section/cms_plugins.py
@@ -17,25 +17,25 @@ class TaccsiteSectionPlugin(StylePlugin):
     # model = TaccsiteSection
     name = _('Section')
 
-    # Copied from djangocms_style sans 'Inline style settings'
-    # FAQ: If user wants to override spacing, they may:
-    #      - use Style plugin (if they have permission)
-    #      - request Design & Dev standardize use case
-    # https://github.com/django-cms/djangocms-style/blob/3.0.0/djangocms_style/cms_plugins.py#L15-L40
-    fieldsets = (
-        (None, {
-            'fields': (
-                'label',
-                ('class_name', 'tag_type'),
-            )
-        }),
-        (_('Advanced settings'), {
-            'classes': ('collapse',),
-            'fields': (
-                'additional_classes',
-                'id_name',
-                'template',
-                'attributes',
-            ),
-        }),
-    )
+    # # Copied from djangocms_style sans 'Inline style settings'
+    # # FAQ: If user wants to override spacing, they may:
+    # #      - use Style plugin (if they have permission)
+    # #      - request Design & Dev standardize use case
+    # # https://github.com/django-cms/djangocms-style/blob/3.0.0/djangocms_style/cms_plugins.py#L15-L40
+    # fieldsets = (
+    #     (None, {
+    #         'fields': (
+    #             'label',
+    #             ('class_name', 'tag_type'),
+    #         )
+    #     }),
+    #     (_('Advanced settings'), {
+    #         'classes': ('collapse',),
+    #         'fields': (
+    #             'additional_classes',
+    #             'id_name',
+    #             'template',
+    #             'attributes',
+    #         ),
+    #     }),
+    # )
